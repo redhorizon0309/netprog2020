39216f4df764f08c99a9b06dd43b6f918c3af7dc
redhorizon0309 <59050113+redhorizon0309@users.noreply.github.com>
2020-05-11 13:25:10 +0700

    [PUSH] Labwork 5
    No bzero(), more send() recv(),
    Kinda working, can send string but some time random character appear out of nowhere.

---
+++ 05.practical.work.client.turn.c
@@ -0,0 +1,75 @@
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <netinet/in.h>
+#include <sys/socket.h> 
+#define MAX 80
+#define PORT 8784
+#define SA struct sockaddr 
+
+void func(int sockfd) 
+{ 
+    char buff[MAX]; 
+    int n; 
+    for (;;) { 
+        
+        printf("Enter message : "); 
+        n = 0; 
+        
+        while ((buff[n++] = getchar()) != '\n'); 
+
+        send(sockfd, buff, sizeof(buff), 0); 
+         
+        if ((strncmp(buff, "exit", 4)) == 0) { 
+            printf("Client Exit...\n"); 
+            break; 
+        }  
+
+        recv(sockfd, buff, sizeof(buff), 0);
+         
+        printf("From Server : %s", buff);
+        
+    } 
+} 
+  
+int main() 
+{ 
+    int sockfd, connfd; 
+    struct sockaddr_in servaddr, cli;
+    struct hostent *h;
+  
+    // socket create and varification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully created..\n"); 
+    bzero(&servaddr, sizeof(servaddr)); 
+    
+    // assign IP, PORT 
+    if ((h=gethostbyname("127.0.0.1")) == NULL) {
+        printf("unknow host \n");
+    }
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET; 
+    memcpy((char *) &servaddr.sin_addr.s_addr, h->h_addr_list[0], h->h_length);
+    servaddr.sin_port = htons(PORT); 
+  
+    // connect the client socket to server socket 
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) < 0) { 
+        printf("connection with the server failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("connected to the server..\n"); 
+  
+    // function for chat 
+    func(sockfd); 
+  
+    // close the socket 
+    close(sockfd); 
+} 
